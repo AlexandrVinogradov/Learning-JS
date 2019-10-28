a195a181c122010bcd878c0e2a3c12bb0f1b7600
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-10-28 19:50:24 +0300

    must have methods of array

--- concepts/12_mmethods__of_array.js
+++ concepts/12_mmethods__of_array.js
@@ -10,14 +10,27 @@ const items = [
     {name: 'Keyboard', price: 25  }
 ]
 
-const itemNames = items.map(i => { // map return new array
+const itemNames = items.map(i => {                                                            // map return new array
     return i.name 
 }); console.log(itemNames);
 
-const filteredItems = items.filter(i => i.price <= 100);  console.log(filteredItems);  // map return new array
-const foundItem = items.find(i => i.name === 'Book');     console.log(foundItem);  // find return value first detected element || undefined
- 
+const filteredItems = items.filter(i => i.price <= 100);    console.log(filteredItems);       // map return new array
+const foundItem = items.find(i => i.name === 'Book');       console.log(foundItem);           // find return value first detected element || undefined
+items.forEach(i => console.log(i.price))                                                      // perform function to every element onece
+const hasInexpensiveItems = items.some(i => i.price <=10);  console.log(hasInexpensiveItems); // return true/false (at least one value)
+const allMostExpensive = items.every(i => i.price >=1000);  console.log(hasInexpensiveItems); // return true/false (if all elements satisfy the condition)
+const total = items.reduce((summ, i) => i.price + summ, 0); console.log(total);               // summ without additional variable 
+
+const items2 = [1, 2, 3, 4, 5] 
+const includesTwo = items2.includes(0);                     console.log(includesTwo);         // return true/false array contain whether value
 
 
 
-// console.log(items);
+
+// const buffer = [
+//     [1, 4, 5],
+//     [4, 4, 5],
+//     [1, 4, 7],
+// ]
+
+// const element = buffer.map(i => i.reduce((summ, j) => summ+j), 0); console.log(element);
