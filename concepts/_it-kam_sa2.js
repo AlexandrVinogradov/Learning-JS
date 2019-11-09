e5151e81b71392691777a22285448aa54df6b839
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-11-09 11:45:37 +0300

    stack, LIFO, FIFO

--- concepts/_it-kam_sa2.js
+++ concepts/_it-kam_sa2.js
@@ -1,7 +1,12 @@
-function eventLoop() {
+(function eventLoop() {
     console.log('one');
-    setTimeout( () => {console.log('two')}, 0)
     setTimeout( () => {console.log('three')}, 1000)
+    setTimeout( () => {console.log('two')}, 0)
     console.log('four');
-}
-eventLoop()
+})()
+// queue FIFO  .pop()
+// stack LIFO
+// pop():     Remove an item from the end of an array.
+// push():    Add items to the end of an array.
+// shift():   Remove an item from the beginning of an array.
+// unshift(): Add items to the beginning of an array.
